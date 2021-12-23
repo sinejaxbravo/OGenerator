26d7976ba9d9a648c34cc6c561db0683e3da133c
Stuart Burgdoerfer <57853652+sinjaxbravo@users.noreply.github.com>
2021-12-23 03:45:40 -0500

    Added class to manipulate images and stitch them. Also removes the background.

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-from DB import DB
-from NeuralNet import NeuralNet
-from Scraper import Scraper
+import os
+import cv2
+import FormatPhoto
+
+shirts = []
+pants = []
+saved = ['IMG_0663.jpg', 'IMG_0655.jpg', 'IMG_0664.jpg']
+for filename in os.listdir("clothes/shirts"):
+    shirts.append(filename)
+
+for filename in os.listdir("clothes/pants"):
+    pants.append(filename)
 
 
-net = NeuralNet()
-db = DB()
-# scraper = Scraper()
+print(pants, "    shirts ", shirts)
 
-print("Hello fucker")
+for pant in pants:
+    for shirt in shirts:
+        p = pant
+        s = shirt
+        s = FormatPhoto.makeImage(f"clothes/shirts/{s}")
+        p = FormatPhoto.makeImage(f"clothes/pants/{p}")
+        s = FormatPhoto.rescaleImage(s)
+        p = FormatPhoto.rescaleImage(p)
+
+        # if p not in saved:
+        #     cv2.imwrite(f"clothes/pairs/{pant}", p)
+        # if s not in saved:
+        #     cv2.imwrite(f"clothes/pairs/{pant}", s)
+
+        s = FormatPhoto.noiseReduction(s)
+        p = FormatPhoto.noiseReduction(p)
+        # if p not in saved:
+        #     cv2.imwrite(f"clothes/pairs/{pant}", p)
+        # if s not in saved:
+        #     cv2.imwrite(f"clothes/pairs/{pant}", s)
+
+        s = FormatPhoto.findAndCut(s, "shirt")
+        p = FormatPhoto.findAndCut(p, "")
+        # if p not in saved:
+        #     cv2.imwrite(f"clothes/pairs/{pant}", p)
+        # if s not in saved:
+        #     cv2.imwrite(f"clothes/pairs/{pant}", s)
+        newPic = FormatPhoto.stitch(s, p)
+        name = pant[0:pant.index(".")] + shirt[0:shirt.index(".")]
+        cv2.imwrite(f"clothes/pairs/{name}.jpg", newPic)
+
+
+
