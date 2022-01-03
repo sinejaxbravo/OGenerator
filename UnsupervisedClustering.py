66d4703971534c6b16ac53aa2f1c77172495b2cf
Stuart Burgdoerfer <57853652+sinjaxbravo@users.noreply.github.com>
2022-01-02 22:48:53 -0500

    Added UnsupervisedClustering.py

--- UnsupervisedClustering.py
+++ UnsupervisedClustering.py
@@ -53,7 +53,7 @@ def Affinity(data, labels, mode="x", title="Affinity"):
     print(clusters)
     if mode != "x":
         for i in range(X.shape[0]):
-            if X[i, 0] < np.average(X[:, 0])/6 and X[i, 1] > np.average(X[:, 1])/6:
+            if X[i, 0] < np.average(X[:, 0]) * 1.25 and X[i, 1] > np.average(X[:, 1]) * 1.25:
                 cut.append(labels[i])
             if labels[i] in fash:
                 plt.scatter(X[i, 0], X[i, 1], c="red")
