3c27bb3d41166318a08428470a30424acbaa70b8
Stuart Burgdoerfer <57853652+sinjaxbravo@users.noreply.github.com>
2022-01-06 17:30:25 -0500

    Set up the database. and changed a lot of file in main.

---
+++ Directories.py
@@ -0,0 +1,20 @@
+path_pant = "C:\\Users\\stuar\\Desktop\\PyProjects\\clothes\\pant\\"
+path_shirt = "C:\\Users\\stuar\\Desktop\\PyProjects\\clothes\\shirt\\"
+path_outfit = "C:\\Users\\stuar\\Desktop\\PyProjects\\clothes\\outfit\\"
+path_pair = "C:\\Users\\stuar\\Desktop\\PyProjects\\clothes\\pair\\"
+path_shoe = "C:\\Users\\stuar\\Desktop\\PyProjects\\clothes\\shoe\\"
+path_coat = "C:\\Users\\stuar\\Desktop\\PyProjects\\clothes\\coat\\"
+path2 = "C:\\Users\\stuar\\Desktop\\TrainingData\\dualclass"
+dirtrain = "/Users/stuar/Desktop/TrainingData/dualclass/train"
+dirtest = "/Users/stuar/Desktop/TrainingData/dualclass/test"
+dir_large = "/Users/stuar/Desktop/TrainingData/unsupervised"
+dir_one = "/Users/stuar/Desktop/TrainingData/unsup"
+dir_pred = "clothes/pair"
+
+dir_1 = "./clothes/predicted_1"
+dir_15 = "./clothes/predicted_1dot5"
+dir_5 = "./clothes/predicted_dot5"
+dir_zip = "./clothes/predicted_zip"
+output = {1: dir_1, 1.5: dir_15, .5: dir_5, 0: dir_zip}
+paths = {"pant": path_pant, "shirt": path_shirt, "outfit": path_outfit, "pair": path_pair, "shoe": path_shoe,
+         "coat": path_coat}
--- Scraper.py
+++ Scraper.py
@@ -16,9 +16,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import urllib.request
 
-
-
-#https://medium.com/geekculture/scraping-images-using-selenium-f35fab26b122
+# https://medium.com/geekculture/scraping-images-using-selenium-f35fab26b122
 path = "C:\Program Files (x86)\Google\chromedriver.exe"
 photofolder = "/Users/stuar/Desktop/TrainingData/FashionGen"
 
@@ -26,27 +24,25 @@ urbanoutfitters = "https://www.urbanoutfitters.com/womens-tops?page=8"
 
 # Reiss works! https://www.reiss.com/us/mens/coats-jackets/jackets/
 # Best website yet!!! SSENSE
+mens = "https://www.ssense.com/en-us/men/designers/connor-mcknight/clothing"
 
-site = "https://www.ssense.com/en-us/men/designers/diesel/clothing?page=2"
-x = 21200
+women = "https://www.ssense.com/en-us/women/clothing?page=8"
 
+x = 24200
+pg = 3
 s = Service(path)
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
+options.add_argument(
+    f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(options=options, service=s)
 
-
-temp = urbanoutfitters
-print(site)
-driver.get(site)
-# print(driver.page_source)
-time.sleep(3)
+driver.get(mens)
 
 # visited = []
 #
@@ -56,7 +52,6 @@ time.sleep(3)
 #     if s not in visited:
 #         visited.append(s)
 #         for x in s.find_elements(By.CLASS_NAME, "c-pwa-tile-tiles")
-
 
 
 # images = driver.find_elements(By.TAG_NAME, "img")
@@ -74,14 +69,13 @@ for g in range(500):
     current += height
     print(g)
 
-
 time.sleep(2)
 print("Parsing")
-print(len(images))
+# print(len(images))
 
 for i in images:
     href = i.get_attribute("srcset")
-    dest = (path+"mens_" + str(x)+".jpg")
+    dest = (path + "mens_" + str(x) + ".jpg")
     # print(i.get_attribute("class"))
     # print("photo: ", dest)
     # urllib.request.urlretrieve(href, dest)
@@ -94,13 +88,19 @@ for i in images:
     except:
         print("Image error")
     x += 1
+    # time.sleep(3)
+    # if pg < 10:
+    #     site = site[0:-1] + str(pg)
+    #
+    # elif pg < 100:
+    #     site = site[0:-2] + str(pg)
+    # else:
+    #     site = site[0:-3] + str(pg)
+    # pg += 1
 
+quit()
+print("STOPPED AT: ", pg)
 # src = img.get_attribute('src')
 # print(src)
 # # download the image
 # urllib.urlretrieve(src, "captcha.png")
-
-time.sleep(2)
-
-
-
